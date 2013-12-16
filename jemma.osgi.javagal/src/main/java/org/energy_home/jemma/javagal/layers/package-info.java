dcb3241af6c70e35454957bc89386dec48bd9146
Marco Nieddu <marco.nieddu@consoft.it>
2013-12-16 09:57:22 +0100

    Changed Licence year

--- jemma.osgi.javagal/src/main/java/org/energy_home/jemma/javagal/layers/package-info.java
+++ jemma.osgi.javagal/src/main/java/org/energy_home/jemma/javagal/layers/package-info.java
@@ -1,6 +1,6 @@
 /**
  * This file is part of JEMMA - http://jemma.energy-home.org
- * (C) Copyright 2010 Telecom Italia (http://www.telecomitalia.it)
+ * (C) Copyright 2013 Telecom Italia (http://www.telecomitalia.it)
  *
  * JEMMA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License (LGPL) version 3
@@ -13,5 +13,4 @@
  * GNU Lesser General Public License (LGPL) for more details.
  *
  */
-
 package org.energy_home.jemma.javagal.layers;
