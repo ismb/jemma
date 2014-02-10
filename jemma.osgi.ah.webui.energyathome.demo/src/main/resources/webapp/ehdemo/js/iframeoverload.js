fe06a81c2ea9b04133576202e3497bb3f10acbbf
Davide Toppazzini <tophans@gmail.com>
2014-02-10 11:48:35 +0100

    Add Report Page for demo vision
    The functionality of reports currently being developed in the Cloud is
    put in this release to allow an overview of the service in Demo mode,
    then no communication with the DB, but with fake data.

--- jemma.osgi.ah.webui.energyathome.demo/src/main/resources/webapp/ehdemo/js/iframeoverload.js
+++ jemma.osgi.ah.webui.energyathome.demo/src/main/resources/webapp/ehdemo/js/iframeoverload.js
@@ -1,13 +1,24 @@
 var iFrameOverload = {
-	MODULE : "Overload"
+	MODULE : "Overload",
+	linkDemo: './reportDemo/overload.html',
+	linkProduction: 'https://trial.energy-home.it/ehreport/overload.html'
 }
-
-alert('iFrameOverload');
 
 iFrameOverload.Init = function () {
 	$("#iframeOverload").show();
+	
+	var powerThreshold = Define.home["contatoreOk"][Main.contatore];
+	var enablePV = (Main.enablePV) ? 'yes' : 'no';
+	
+	var linkToUse = '';
+	
+	if (InterfaceEnergyHome.mode > 0) {
+		linkToUse = iFrameOverload.linkProduction;
+	} else {
+		linkToUse = iFrameOverload.linkDemo;
+	} 
 
-    $("#iframeOverload").html("<iframe id='iframeOver' src='http://10.38.0.1/ehreport/index.html' width='100%' height='100%' onload='iframeOverload.VisIFrame()' frameborder='0'>Contenuto alternativo per i browser che non leggono gli iframe.</iframe>");
+    $("#iframeOverload").html("<iframe id='iframeOver' src='"+linkToUse+"?power="+powerThreshold+"&enablePV="+enablePV+"' width='100%' height='100%' onload='iFrameOverload.VisIFrame()' frameborder='0'>Contenuto alternativo per i browser che non leggono gli iframe.</iframe>");
 	showSpinner();
 }
 
