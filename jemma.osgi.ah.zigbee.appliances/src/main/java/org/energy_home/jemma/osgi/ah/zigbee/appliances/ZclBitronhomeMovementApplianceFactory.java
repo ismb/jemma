59b480250214f1f7f29237f44bb2922a3809e9a6
ivangrimaldi <ivan.grimaldi.to@gmail.com>
2014-03-28 11:33:32 +0100

    Added drivers for Magnet Sensor and Movement Sensor from Bitron

---
+++ jemma.osgi.ah.zigbee.appliances/src/main/java/org/energy_home/jemma/osgi/ah/zigbee/appliances/ZclBitronhomeMovementApplianceFactory.java
@@ -0,0 +1,35 @@
+package org.energy_home.jemma.osgi.ah.zigbee.appliances;
+
+import java.util.Dictionary;
+
+import org.energy_home.jemma.ah.hac.ApplianceException;
+import org.energy_home.jemma.ah.hac.IApplianceDescriptor;
+import org.energy_home.jemma.ah.hac.lib.Appliance;
+import org.energy_home.jemma.ah.hac.lib.ApplianceDescriptor;
+import org.energy_home.jemma.ah.hac.lib.DriverApplianceFactory;
+import org.osgi.service.device.Driver;
+
+public class ZclBitronhomeMovementApplianceFactory extends DriverApplianceFactory implements Driver{
+
+	public static final String APPLIANCE_TYPE = "org.energy_home.jemma.ah.zigbee.bitronhome.magnet";
+	public static final String APPLIANCE_FRIENDLY_NAME = "bitronhome magnet sensor";
+	public static final String DEVICE_TYPE = "ZigBee";
+
+	public static final IApplianceDescriptor APPLIANCE_DESCRIPTOR = new ApplianceDescriptor(APPLIANCE_TYPE, null,
+			APPLIANCE_FRIENDLY_NAME);
+
+	public IApplianceDescriptor getDescriptor() {
+		return APPLIANCE_DESCRIPTOR;
+	}
+
+	public Appliance getInstance(String pid, Dictionary config) throws ApplianceException {
+		return new ZclBitronhomeMovementAppliance(pid, config);
+	}
+
+	public String deviceMatchFilterString() {
+		//String result = "(&(DEVICE_CATEGORY=ZigBee)(zigbee.device.eps.number=1)(zigbee.device.profile.id=260)(zigbee.device.device.id=6)(zigbee.device.manufacturer.id=0))";
+		String result="";
+		return result; 
+		
+	}
+}
