bb3e43ef2db38d04de6ceaa413e02631422750ff
Marco Nieddu <marco.nieddu@consoft.it>
2014-04-04 17:30:09 +0200

    Added wrong files

---
+++ jemma.osgi.javagal.rest/src/main/java/org/energy_home/jemma/javagal/rest/RestApsMessageListener.java
@@ -0,0 +1,149 @@
+/**
+ * This file is part of JEMMA - http://jemma.energy-home.org
+ * (C) Copyright 2013 Telecom Italia (http://www.telecomitalia.it)
+ *
+ * JEMMA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) version 3
+ * or later as published by the Free Software Foundation, which accompanies
+ * this distribution and is available at http://www.gnu.org/licenses/lgpl.html
+ *
+ * JEMMA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License (LGPL) for more details.
+ *
+ */
+package org.energy_home.jemma.javagal.rest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.energy_home.jemma.javagal.rest.util.ClientResources;
+import org.energy_home.jemma.javagal.rest.util.Util;
+import org.energy_home.jemma.zgd.APSMessageListener;
+import org.energy_home.jemma.zgd.MessageListener;
+import org.energy_home.jemma.zgd.jaxb.APSMessageEvent;
+import org.energy_home.jemma.zgd.jaxb.Callback;
+import org.energy_home.jemma.zgd.jaxb.Info;
+import org.energy_home.jemma.zgd.jaxb.InterPANMessageEvent;
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.resource.ClientResource;
+
+/**
+ * Implementation of {@code APSMessageListener} interface for the Rest server.
+ * <p>
+ * Rest clients interested to listen to Aps messages, resister themselves
+ * indicating an uri, here called urilistener, where they are listening for
+ * incoming notifications. In practice the clients opens an http server at the
+ * urilistener uri where this class can {@code POST} incoming notifications.
+ * 
+ * @author 
+ *         "Ing. Marco Nieddu <marco.nieddu@consoft.it> or <marco.niedducv@gmail.com> from Consoft Sistemi S.P.A.<http://www.consoft.it>, financed by EIT ICT Labs activity SecSES - Secure Energy Systems (activity id 13030)"
+ * 
+ */
+public class RestApsMessageListener implements APSMessageListener {
+
+	private Log log = LogFactory.getLog(RestApsMessageListener.class);
+	private Long CalbackIdentifier = -1L;
+	private Callback callback;
+	private String urilistener;
+	private ClientResources clientResource;
+	private final Context context;
+	private PropertiesManager _PropertiesManager;
+
+	/**
+	 * Creates a new instance with a given callback, urilistener and client
+	 * resource.
+	 * <p>
+	 * Rest clients interested to listen to Aps messages, resister themselves
+	 * indicating an uri, here called urilistener, where they are listening for
+	 * incoming notifications. In practice the clients opens an http server at
+	 * the urilistener uri where this class can {@code POST} incoming
+	 * notifications.
+	 * 
+	 * @param callback
+	 *            the callback.
+	 * @param urilistener
+	 *            the urilistener.
+	 * @param _clientResource
+	 *            the client resource.
+	 */
+	public RestApsMessageListener(Callback callback, String urilistener, ClientResources _clientResource, PropertiesManager __PropertiesManager) {
+		super();
+		this.callback = callback;
+		this.urilistener = urilistener;
+		this.clientResource = _clientResource;
+		this.context = new Context();
+		this._PropertiesManager = __PropertiesManager;
+		context.getParameters().add("socketTimeout", ((Integer) (_PropertiesManager.getHttpOptTimeout() * 1000)).toString());
+
+	}
+
+	/**
+	 * Notification of an incoming Aps message.
+	 */
+	synchronized public void notifyAPSMessage(final APSMessageEvent message) {
+
+		if (urilistener != null) {
+			Thread thr = new Thread() {
+				@Override
+				public void run() {
+					try {
+
+						ClientResource resource = new ClientResource(context, urilistener);
+						Info info = new Info();
+						Info.Detail detail = new Info.Detail();
+						detail.setAPSMessageEvent(message);
+						info.setDetail(detail);
+						info.setEventCallbackIdentifier(CalbackIdentifier);
+						String xml = Util.marshal(info);
+						if (_PropertiesManager.getDebugEnabled())
+							log.info(xml);
+						resource.post(xml, MediaType.APPLICATION_XML);
+						resource.release();
+						resource = null;
+						clientResource.resetCounter();
+					} catch (Exception e) {
+						clientResource.addToCounterException();
+
+					}
+				}
+			};
+			thr.start();
+		}
+
+	}
+
+	/**
+	 * Gets the urilistener.
+	 * 
+	 * @return the urilistener.
+	 */
+	public String getUrilistener() {
+		return urilistener;
+	}
+
+	/**
+	 * Gets the callback.
+	 * 
+	 * @return the callback.
+	 */
+	public Callback getCallback() {
+		return callback;
+	}
+
+	/**
+	 * Sets the callback id.
+	 * 
+	 * @param id
+	 *            the callback id to set.
+	 */
+	public void setCallBackId(Long id) {
+
+		CalbackIdentifier = id;
+
+	}
+
+
+
+}
